e155da9e4fd0604dfeb7ec7a96119ab4b570a540
Ravindu26881 <85988392+Ravindu26881@users.noreply.github.com>
2021-10-20 12:42:30 +0530

    api

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about spage</h1>
+    <button @click="api">sss</button>
+    <br>
+    <div style="display: flex;
+                flex-direction: column;">
+      <div style="display: flex;
+                  flex-direction: row;
+                  justify-content: space-between;"
+            v-for="item in list" :key="item">
+        <div>{{list[0].Nation}}</div>
+        <div>{{list[0].Year}}</div>
+        <div>{{list[0].Population}}</div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'About',
+  data () {
+    return {
+      list: {}
+    }
+  },
+  methods: {
+    api () {
+      console.log('clicked')
+      const options = {
+        method: 'GET',
+        url: 'https://datausa.io/api/data?drilldowns=Nation&measures=Population',  
+      };
+      axios.request(options).then((response) => {
+        console.log(response.data.data)
+        this.list = response.data.data
+        console.log('list' , this.list)
+      }).catch((error) => {
+        console.error(error)
+      });
+    }
+  }
+}
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,12 +1,75 @@
 <template>
-  <div></div>
+<div>
+  <div >
+    <h1>Enter your name to calculate your age</h1>
+    <input id="name-input"/>
+  </div>
+
+  <button type="button" class="btn btn-primary" @click="clicked" data-toggle="modal" data-target="#exampleModal">
+  calculate
+</button>
+
+<div style="color: black;" class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel" v-if="nameFieldEmpty === false">Hi {{name}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h4 v-if="nameFieldEmpty === false"> Your age is {{list.age}} </h4>
+        <h4 v-if="nameFieldEmpty === true"> Enter your name to calculate your age </h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   name: 'Settings',
-  components: {
+  data () {
+    return {
+      name: '',
+      list: {},
+      nameFieldEmpty: false
+    }
+  },
+  methods: {
+    clicked () {
+      let inputValue = document.getElementById('name-input').value
+      if (inputValue.length > 0) {
+        this.nameFieldEmpty = false
+        this.name = inputValue
+        document.getElementById('name-input').value = ''
+        this.api()
+      }
+      if (inputValue.length === 0) {
+        this.nameFieldEmpty = true
+      }
+    },
+    api () {
+      console.log('clicked')
+      const options = {
+        method: 'GET',
+        url: 'https://api.agify.io/?name=' + this.name,  
+      };
+      axios.request(options).then((response) => {
+        console.log(response.data)
+        this.list = response.data
+        console.log('list' , this.list)
+      }).catch((error) => {
+        console.error(error)
+      });
+    }
   }
 }
 </script>
